664ffb696e278977e9ba008e38833829b6fef5d8
Ade Putra <poetrajerami@gmail.com>
2018-12-21 16:03:23 +0700

    home pilpres

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="home-wrapper">
-    <div class="container d-flex">
-      <div class="col-md-2 sidebar">
+    <div class="container d-flex main-wrapper">
+      <div class="col-md-3 sidebar">
         <NavSidebarTop></NavSidebarTop>
         <NavSidebarBottom></NavSidebarBottom>
       </div>
       <div class="col-md-6 offset-md-3 main-content">
+        <CardPilpres></CardPilpres>
         <CardJP></CardJP>
       </div>
       <div class="col-md-3 widget-wrapper">
@@ -19,13 +20,15 @@
 import NavSidebarTop from '@/components/NavSidebarTop'
 import NavSidebarBottom from '@/components/NavSidebarBottom'
 import CardJP from '@/components/CardJP'
+import CardPilpres from '@/components/CardPilpres'
 
 export default {
   name: 'Home',
   components: {
     NavSidebarTop,
     NavSidebarBottom,
-    CardJP
+    CardJP,
+    CardPilpres
   }
 }
 </script>
