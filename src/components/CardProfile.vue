b2dad077f5ea074fe7061fd10c3cca8f79b3fc31
Ade Putra <poetrajerami@gmail.com>
2019-01-02 16:10:15 +0700

    verified button

--- src/components/CardProfile.vue
+++ src/components/CardProfile.vue
@@ -10,9 +10,23 @@
         <img src="@/assets/trump.jpg">
         <span>
           <h3>Jelo Caviar, @jelo</h3>
-          <button class="line">
-            <img src="@/assets/icon_setting.svg"> Terverifikasi
+          <div v-if="isVerified" class="line-verified">
+            <svg version="1.1" id="Layer_1" viewBox="0 0 48 48" style="enable-background:new 0 0 48 48;" xml:space="preserve">
+            <path id="Path_1872" class="st0" d="M38.9,24l3.1-4.8l-5.1-2.6l0.3-5.7l-5.7,0.3L28.8,6L24,9.1L19.2,6l-2.6,5.1l-5.7-0.3l0.3,5.7
+              L6,19.2L9.1,24L6,28.8l5.1,2.6l-0.3,5.7l5.7-0.3l2.6,5.1l4.8-3.1l4.8,3.1l2.6-5.1l5.7,0.3l-0.3-5.7l5.1-2.6L38.9,24z M21.2,31
+              l-6.4-6.4l2.1-2.1l4.2,4.2L31,17l2.1,2.1L21.2,31z"/>
+            </svg>
+             Terverifikasi
+          </div>
+          <button v-else class="btn line">
+            <svg version="1.1" id="Layer_1" viewBox="0 0 48 48" style="enable-background:new 0 0 48 48;" xml:space="preserve">
+            <path id="Path_1872" class="st0" d="M38.9,24l3.1-4.8l-5.1-2.6l0.3-5.7l-5.7,0.3L28.8,6L24,9.1L19.2,6l-2.6,5.1l-5.7-0.3l0.3,5.7
+              L6,19.2L9.1,24L6,28.8l5.1,2.6l-0.3,5.7l5.7-0.3l2.6,5.1l4.8-3.1l4.8,3.1l2.6-5.1l5.7,0.3l-0.3-5.7l5.1-2.6L38.9,24z M21.2,31
+              l-6.4-6.4l2.1-2.1l4.2,4.2L31,17l2.1,2.1L21.2,31z"/>
+            </svg>
+             Belum Verifikasi
           </button>
+          
         </span>
         <p>An ordinary netizen who likes to talk a lot. Like, a lot. Who cares anyway!</p>
       </div>
@@ -118,6 +132,11 @@ export default {
   name: 'CardProfile',
   components: {
     ListCardJP
+  },
+  data (){
+    return {
+      isVerified: true
+    }
   }
 }
 </script>
