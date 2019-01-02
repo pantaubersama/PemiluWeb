494a5cbf54c03588d3837a7645f75e7deb3c3187
Rohmad Sasmito <rochmad.26@gmail.com>
2019-01-02 21:19:51 +0700

    Add background in modal question

---
+++ src/components/modal/modal-background.vue
@@ -0,0 +1,20 @@
+<template>
+  <transition name="modal">
+    <div class="modal-mask">
+      <div class="modal-wrapper">
+        <div class="modal-background"></div>
+        <div class="modal-container">
+          <slot name="modal-content">
+            default Modal
+          </slot>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+  export default {
+    name: 'Modal'
+  }
+</script>
