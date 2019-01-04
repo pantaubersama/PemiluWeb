ca4ecbae5315cfc0afec8c8791d028284033a54d
Ade Putra <poetrajerami@gmail.com>
2019-01-04 14:37:07 +0700

    setting connect

--- src/svg/index.js
+++ src/svg/index.js
@@ -7,6 +7,8 @@ import IconTwitter from './icon-twitter.svg'
 import IconFacebook from './icon-facebook.svg'
 import BottomArrow from './bottom-arrow.svg'
 import IconDots from './icon-dots.svg'
+import IconTwitter from './icon-twitter.svg'
+import IconFacebook from './icon-facebook.svg'
 
 export {
   LogoIcon,
@@ -17,5 +19,7 @@ export {
   IconTwitter,
   IconFacebook,
   BottomArrow,
-  IconDots
+  IconDots,
+  IconTwitter,
+  IconFacebook
 }
