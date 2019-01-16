5e7fe50039bd17a64b2fc959feb34802a5018b84
Afief Sholichuddin <apiep.oedin@gmail.com>
2019-01-17 03:32:05 +0700

    Done ubah profile

--- src/pages/Profile/ModalEditProfile.vue
+++ src/pages/Profile/ModalEditProfile.vue
@@ -1,47 +1,54 @@
 <template>
   <modal class="modal-edit-profile">
     <template slot="modal-content">
-      <!-- <div class="modal-header"> -->
       <div class="close-icon" @click="$emit('close')">
         <close-icon/>
-        <!-- </div> -->
       </div>
       <form @submit.prevent="submit($event)">
         <div class="card avatar">
           <div class="user-profile">
-            <img class="profile-thumb" src="@/assets/trump.jpg">
+            <img v-if="avatarURL != null" class="profile-thumb" :src="avatarURL">
+            <img v-else class="profile-thumb" src="~@/assets/user.svg">
           </div>
-          <button class="add-image">Ganti Gambar</button>
+          <button
+            class="add-image"
+            type="button"
+            @click.stop="() => $refs.inputAvatar.click()"
+          >Ganti Gambar</button>
+          <input
+            id="avatar"
+            name="avatar"
+            ref="inputAvatar"
+            type="file"
+            accept="image/*"
+            style="display:none"
+            @change="inputAvatarChanged($event)"
+          >
         </div>
         <div class="card">
           <div class="card-column">
             <label for="name">Nama</label>
-            <input type="text" id="name" name="name" value="Jelo Caviar">
+            <input type="text" id="name" name="name" :value="user.full_name">
           </div>
           <div class="card-column">
             <label for="username">Username</label>
-            <input type="text" id="username" name="username" value="@jelocaviar">
+            <input type="text" id="username" name="username" :value="user.username">
           </div>
           <div class="card-column">
             <label for="description">Deskripsi Tentang Kamu</label>
-            <input
-              type="text"
-              id="description"
-              name="description"
-              value="An ordinary netizen who likes to talk a lot. Like, a lot."
-            >
+            <input type="text" id="description" name="description" :value="user.about">
           </div>
           <div class="card-column">
             <label for="lokasi">Lokasi</label>
-            <input type="text" id="lokasi" name="lokasi" value="Bandung">
+            <input type="text" id="lokasi" name="lokasi" :value="user.location">
           </div>
           <div class="card-column">
             <label for="pendidikan">Pendidikan</label>
-            <input type="text" id="pendidikan" name="pendidikan" value="Bandung">
+            <input type="text" id="pendidikan" name="pendidikan" :value="user.education">
           </div>
           <div class="card-column">
             <label for="pekerjaan">Pekerjaan</label>
-            <input type="text" id="pekerjaan" name="pekerjaan" value="Petani">
+            <input type="text" id="pekerjaan" name="pekerjaan" :value="user.occupation">
           </div>
         </div>
         <input type="submit" value="Simpan" class="btn btn-outline">
@@ -55,10 +62,39 @@ import Modal from '@/layout/Modal'
 import { CloseIcon } from '@/svg/icons'
 export default {
   name: 'ModalEditProfile',
+  props: ['user'],
   components: { Modal, CloseIcon },
+  data() {
+    return {
+      avatarURL: this.user.avatar.medium.url
+    }
+  },
   methods: {
     submit(event) {
-      this.$emit('close')
+      const avatar = Array.from(event.target['avatar'].files).pop()
+      const name = event.target['name'].value.trim()
+      const username = event.target['username'].value.trim()
+      const description = event.target['description'].value
+      const location = event.target['lokasi'].value
+      const pendidikan = event.target['pendidikan'].value
+      const pekerjaan = event.target['pekerjaan'].value
+      this.$emit('submit', {
+        avatarFile: avatar,
+        full_name: name,
+        username,
+        description,
+        location,
+        education: pendidikan,
+        occupation: pekerjaan
+      })
+    },
+    inputAvatarChanged(event) {
+      const file = Array.from(event.target.files).pop()
+      if (this.avatarURL != null) {
+        URL.revokeObjectURL(this.avatarURL)
+      }
+      const url = URL.createObjectURL(file)
+      this.avatarURL = url
     }
   }
 }
