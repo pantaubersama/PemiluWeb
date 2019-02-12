3172fc76749fde585484b068768445c16809ebf6
Rohmad Sasmito <rochmad.26@gmail.com>
2019-02-12 21:46:05 +0700

    Implement feature search janji politik

--- src/services/api/search.js
+++ src/services/api/search.js
@@ -4,7 +4,9 @@ import {
 import Api from '@/services/api/base'
 
 const BASE_URL = process.env.API_BASE_URL || 'https://staging-auth.pantaubersama.com'
+const PEMILU_URL = process.env.API_PEMILU_BASE_URL || 'https://staging-pemilu.pantaubersama.com'
 const api = Api(BASE_URL, () => vueAuth.getToken())
+const apiPemilu = Api(PEMILU_URL, () => vueAuth.getToken())
 
 /* eslint-disable camelcase */
 export const people = ({
@@ -20,4 +22,24 @@ export const people = ({
     }
   })
   .then(resp => resp.data.data.users)
+
+export const janjiPolitiks = ({
+  q,
+  cluster_id,
+  filter_by = 'user_verified_all',
+  page = 1,
+  per_page = 20
+}) => {
+  return apiPemilu
+    .get('/linimasa/v1/janji_politiks', {
+      params: {
+        q,
+        cluster_id,
+        filter_by,
+        page,
+        per_page
+      }
+    })
+    .then(resp => resp.data.data.janji_politiks)
+}
 /* eslint-disable */
