d70b895b7d7831169689a0f61f6b2f12c23aa567
Afief Sholichuddin <apiep.oedin@gmail.com>
2019-03-12 20:00:55 +0700

    Add confirmation modal to incoming debat detail

--- src/pages/wordstadium/detail-incoming.vue
+++ src/pages/wordstadium/detail-incoming.vue
@@ -1,28 +1,52 @@
 <template>
   <layout-timeline>
     <template slot="main-content">
-      <debat-detail
-        title="Open Challenge"
-        :debat="debat"
-        :with-source="false"
-        :with-opponent="false"
-        :challenger="challenger"
-        :opponent="opponent"
-        banner-text1="Ini adalah Open Challenge,"
-        banner-text2="Terima tantangan ini?"
-      >
-      <template slot="debat-subcontent">
-        <button type="button" class="accept-challenge">
-          <i class="icon icon-accept-challenge"></i>
-          <div class="text">Terima tantangan</div>
-        </button>
-      </template>
-      </debat-detail>
+      <div>
+        <debat-detail
+          title="Open Challenge"
+          :debat="debat"
+          :with-source="false"
+          :with-opponent="false"
+          :challenger="challenger"
+          :opponent="opponent"
+          banner-text1="Ini adalah Open Challenge,"
+          banner-text2="Terima tantangan ini?"
+        >
+        <template slot="debat-subcontent">
+          <button type="button" class="accept-challenge"
+            @click.prevent="isModalOpen = !isModalOpen">
+            <i class="icon icon-accept-challenge"></i>
+            <div class="text">Terima tantangan</div>
+          </button>
+        </template>
+        </debat-detail>
+        <modal-layout v-if="isModalOpen" class="modal-confirm">
+          <div slot="modal-content" class="modal-confirm-challenge">
+            <div class="modal-body modal-body--confirm-challenge">
+              <slot name="content">
+                <img src="~@/assets/img-confirm-challenge.png" alt="img-confirm-challenge" class="illustration">
+                <div class="text">
+                  Kamu akan menerima tantangan dari
+                  <b>@raja_kampreta</b> untuk berdebat
+                  sesuai dengan detail yang tertera.
+                  Tindakan ini tidak bisa dibatalkan.
+                  Apakah kamu yakin?
+                </div>
+                <div class="buttons">
+                  <button type="button" @click="isModalOpen = false">Kembali</button>
+                  <button type="button" @click="isModalOpen = false" class="teal">Ya</button>
+                </div>
+              </slot>
+            </div>
+          </div>
+        </modal-layout>
+      </div>
     </template>
   </layout-timeline>
 </template>
 
 <script>
+import ModalLayout from '@/layout/Modal'
 import LayoutTimeline from '@/layout/Timeline'
 import DebatDetail from '@/components/debat-detail'
 import dummyDebat from '@/pages/wordstadium/dummy-debat.json'
@@ -56,7 +80,13 @@ export default {
   name: 'DebatDetailIncoming',
   components: {
     LayoutTimeline,
-    DebatDetail
+    DebatDetail,
+    ModalLayout
+  },
+  data() {
+    return {
+      isModalOpen: false
+    }
   },
   computed: {
     debat() { return dummyDebat },
@@ -106,4 +136,62 @@ export default {
     letter-spacing: 0.5px
     text-align: center
     color: #ffffff
+
+
+.modal-confirm
+  & /deep/ .modal-container
+    padding: 0
+    width: 318px
+    height: 337px
+    border-radius: 16px
+    overflow: hidden
+  & /deep/ .modal-confirm-challenge
+    height: 100%
+    width: 100%
+  & /deep/ .modal-body
+    display: flex
+    flex-direction: column
+    align-items: center
+    height: 100%
+    padding: 0
+  img.illustration
+    flex: 0 0 160px
+    height: 160px
+    width: 160px
+    margin-top: 40px
+  .text
+    flex: 1
+    text-align: center
+    font-size: 15px
+    font-weight: 500
+    line-height: 1.2
+    color: #5c5d62
+    padding: 0 20px
+    b
+      font-weight: bold
+      color: #212121
+  .buttons
+    flex: 0 0 48px
+    height: 48px
+    display: flex
+    width: 100%
+    button
+      flex: 1
+      display: flex
+      justify-content: center
+      align-items: center
+      text-align: center
+      background: #f4f4f4
+      border: 0
+      font-size: 14px
+      line-height: 0.86
+      letter-spacing: 1.4px
+      text-align: center
+      color: #212121
+      text-transform: uppercase
+      cursor: pointer
+      &.teal
+        background: #08bda8
+        color: white
+
 </style>
--- src/services/api/notification.js
+++ src/services/api/notification.js
@@ -2,6 +2,7 @@ import { http } from '@/services/http'
 
 export default {
   getNotification(page, perPage, cb, errorCb) {
+    // TODO: Add fallback incase of env API_NOTIFICATION_URL are undefined
     http.api('get', process.env.API_NOTIFICATION_URL + '/dashboard/v1/notifications', {
       page: page,
       per_page: perPage
