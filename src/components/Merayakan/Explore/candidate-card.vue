a5a127d9c29a463286f46750189e03f0045b88b6
Afief Sholichuddin <apiep@users.noreply.github.com>
2019-04-16 00:34:10 +0700

    Done some detail page (#220)

---
+++ src/components/Merayakan/Explore/candidate-card.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="card transparent">
+    <div class="vote-party">
+      <div class="thumb">
+        <img v-if="candidate.logo == null" src="https://placeimg.com/240/240/tech/1" :alt="candidate.acronym">
+        <img v-else :src="candidate.logo.thumbnail.url" :alt="candidate.acronym" :title="candidate.acronym">
+      </div>
+      <div class="party-desc">
+        <p>{{candidate.name}}</p>
+        <span>No Urut {{candidate.serial_number}}</span>
+      </div>
+      <div class="vote-result">
+        <div class="percentage">70%</div>
+        <span>|</span>
+        <div class="vote">1.000.000 suara</div>
+      </div>
+    </div>
+    <div class="vote-candidate">
+      <ul>
+        <li v-for="(item, index) in candidate.candidates" :key="item.id">
+          <div class="candidate-name">{{index + 1}}. {{item.name}}</div>
+          <div class="vote-result">
+            <div class="percentage">70%</div>
+            <span>|</span>
+            <div class="vote">1.000.000 suara</div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CandidateCard',
+  props: ['candidate']
+}
+</script>
+
+
+<style lang="sass" scoped>
+.vote-party
+  background-color: #ececec
+  color: #111111
+  display: flex
+  align-items: center
+  padding: 17px 20px
+  font-weight: bold
+  font-size: 12px
+  .thumb
+    width: 50px
+    height: 50px
+    img
+      width: 100%
+      height: 100%
+      object-fit: contain
+      object-position: left center
+  .party-desc
+    padding: 0 20px
+    flex: 1
+    p
+      margin-bottom: 0
+      font-size: 13px
+      font-family: 'Bw Modelica'
+    span
+      font-weight: normal
+      color: #4f4f4f
+
+.vote-result
+  display: flex
+  align-items: center
+  .percentage
+    color: #9b0012
+  span
+    margin: 0 10px
+
+.vote-candidate
+  li
+    display: flex
+    align-items: center
+    padding: 10px 20px
+    font-weight: bold
+    font-size: 12px
+    border-top: solid 1px #ececec
+    &:first-child
+      border-top: none
+    .candidate-name
+      flex: 1
+</style>
--- src/components/Merayakan/Explore/index.vue
+++ src/components/Merayakan/Explore/index.vue
@@ -1,19 +1,21 @@
 <template>
   <Explore>
     <template slot="main-content" slot-scope="props">
-      <!-- <explore-presiden v-if="this.$route.params.type === 'presiden'" ></explore-presiden> -->
       <explore-presiden v-if="props.type === 'presiden'" :query="query"></explore-presiden>
+      <explore-dprd-kabupaten v-if="props.type === 'dprd-kabupaten'"></explore-dprd-kabupaten>
     </template>
   </Explore>
 </template>
 
 <script>
 import Explore from '@/components/Merayakan/ExploreLayout'
+
 export default {
   name: 'MerayakanExplore',
   components: {
     Explore,
     ExplorePresiden: () => import('@/components/Merayakan/Explore/presiden'),
+    ExploreDprdKabupaten: () => import('@/components/Merayakan/Explore/dprd-kabupaten')
   },
   computed: {
     query() {
